81b5cd8490407bc5ecd7f3d1045df4591884ed20
israel mvono <israelmvono@gmail.com>
2022-07-08 15:44:56 +0300

    08/07/2022 commit

--- dist/nav.js
+++ dist/nav.js
@@ -14,7 +14,6 @@
 //     }
 // })
 
-
 // ********** close links ***********
 const navToggle = document.querySelector('.nav-toggle')
 const linksContainer = document.querySelector('.links-container')
@@ -29,4 +28,60 @@ navToggle.addEventListener('click',(e)=>{
     }else{
         linksContainer.style.height = 0
     }
+})
+
+
+// *********** fixed navbar **********
+const navBar = document.getElementById('nav')
+const topLink = document.querySelector('.top-link')
+window.addEventListener('scroll',()=>{
+    const scrollHeight = window.pageYOffset
+    const navHeight = nav.getBoundingClientRect().height
+    
+    if(scrollHeight > navHeight){
+        navBar.classList.add('fixed-nav')
+    }else{
+        navBar.classList.remove('fixed-nav')
+    }
+
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link')
+    }else{
+        topLink.classList.remove('show-link')
+    }
+})
+
+// ********** smooth scroll **********
+
+//select links
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+scrollLinks.forEach((link)=>{
+    //prevent default
+    link.addEventListener('click',(e)=>{
+        e.preventDefault()
+
+        //navigate to a specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1)
+        const element = document.getElementById(id)
+
+        //calculating heights
+        const navHeight = navBar.getBoundingClientRect().height
+        const containerHeight = linksContainer.getBoundingClientRect().height
+        const fixedNav = navBar.classList.contains('fixed-nav')
+        let position = element.offsetTop - navHeight
+
+        if(!fixedNav){
+            position = position - navHeight
+        }
+        if(navHeight > 82){
+            position = position + containerHeight
+        }
+        window.scrollTo({
+            left: 0,
+            top: position,
+        })
+
+        linksContainer.style.height = 0
+    })
 })
